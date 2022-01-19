cf437e620193ce0830292c5d6167a24e960543bc
heikey <joaofialho123123@gmail.com>
2022-01-19 15:47:57 -0300

    Test and Main File: added they

---
+++ index.js
@@ -0,0 +1,2 @@
+module.exports.Client = require("./structs/Client")
+module.exports.Intents = require("./structs/Intents")
---
+++ test.js
@@ -0,0 +1,4 @@
+const LIB = require('./index.js');
+let token = 'NzE4OTEyMjM0OTM5NzQ0MjU2.XtvxMQ.u4ak5RlQ0IAGW-ll9z0GC8Qpr9I'
+
+const bot = new LIB.Client(token,)
